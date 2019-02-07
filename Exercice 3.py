89f7ecbf1c83c4c8d77ac77cb147029b344a4a5e
Michelle Bausager <Bausager.m@live.dk>
2019-02-07 15:48:21 +0100

    New

---
+++ Exercice 3.py
@@ -0,0 +1,41 @@
+'''
++ = plus, addition
+- = minus, subtraction
+/ = slash, divid
+* = asterisk, times
+% = percent, 
+< = less-than
+> = greater-than
+<= = les-than-equal
+>= = greater-thanequal
+'''
+
+print("I will now count my chickens:")
+
+print("Hens", 25+30/6)
+print("Roosters", 100-25*3%4)
+
+print("Now I will count the eggs:")
+
+print(3+2+1-5+4%2-1/4+6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3+2<5-7)
+
+print("What is 3+2",3+2)
+
+print("What is 5-7?", 5-7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it geater?",5>-2)
+print("Is it geater or equal?", 5>=-2)
+print("Is it less or equal?", 5<=-2)
+
+##Study Drill
+
+
+print(3%5)
---
+++ Exercise 1.py
@@ -0,0 +1,11 @@
+#print ("Hallo World!")
+#print ("Hallo Again")
+#print ("I like typing this.")
+#print ("This is fun.")
+#print ('Yay! Printing')
+#print ("I'd much rather you 'not'")
+#print ('I "said" do not touch this.')
+
+##Study Drills
+
+print("My Line")
