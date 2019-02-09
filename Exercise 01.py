f502b18639372cfa7c5894d23e80f418c7a7ea17
Michelle Bausager <Bausager.m@live.dk>
2019-02-09 11:58:23 +0100

    Review of it all

--- Exercise 01.py
+++ Exercise 01.py
@@ -1,11 +1,11 @@
-#print ("Hallo World!")
-#print ("Hallo Again")
-#print ("I like typing this.")
-#print ("This is fun.")
-#print ('Yay! Printing')
-#print ("I'd much rather you 'not'")
-#print ('I "said" do not touch this.')
+# print ("Hallo World!")
+# print ("Hallo Again")
+# print ("I like typing this.")
+# print ("This is fun.")
+# print ('Yay! Printing')
+# print ("I'd much rather you 'not'")
+# print ('I "said" do not touch this.')
 
-##Study Drills
+#                                                                Study Drills
 
 print("My Line")
