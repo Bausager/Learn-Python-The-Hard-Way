60bca0cf5ffacce31a1473d493799f6c2f4c9670
Michelle Bausager <Bausager.m@live.dk>
2019-02-10 12:49:13 +0100

    Remodel

--- Exercise14.py
+++ Exercise14.py
@@ -5,3 +5,20 @@ prompt = '> '
 
 print(f"Hi {user_name}, I'm the {script} script")
 print("I'd like to ask you a few questions")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print(f"""
+Alrigth, so you said {likes} about likeing me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+""")
+
+# Study Drill
+# Pas
