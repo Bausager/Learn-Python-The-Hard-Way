9105c8767c35cbaa8eaf2a5a4ed14c85462bc532
Michelle Bausager <Bausager.m@live.dk>
2019-02-10 09:49:35 +0100

    New Exercises

---
+++ Exercise 05.py
@@ -0,0 +1,64 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffe.")
+
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+							# Study Drill
+
+							# 1 Study Drill
+name = 'Zed A. Shaw'
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffe.")
+
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+
+
+							# 2 Study Drill
+name = 'Zed A. Shaw'
+age = 35
+height_inches = 74
+height_meters = 74 / 39.370
+weight_pounds = 180
+weight_kilograms = weight_pounds / 2.2046226218
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height_meters} inches tall.")
+print(f"He's {weight_kilograms} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffe.")
+
+
+total = round(age + height + weight)
+print(f"If I add {age}, {height_meters}, and {weight_kilograms} I get {total}.")
---
+++ Exercise 06.py
@@ -0,0 +1,104 @@
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}")
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
+
+								# Study Drill
+
+					# 1 Study Drill
+# The number of defirend types of people.
+types_of_people = 10
+
+# Defind and refers to the types of people, so it prints it.
+# The f"" makes it posiable.
+x = f"There are {types_of_people} types of people."
+
+# Defind binary.
+binary = "binary"
+# Defind do_not
+do_not = "don't"
+
+# Prints and refers to the binary and do_not, so it prints it.
+y = f"Those who know {binary} and those who {do_not}."
+
+# Prints x and y. Since it is defind as a string it is posiable.
+print(x)
+print(y)
+
+# Prints and refers to a string
+print(f"I said: {x}")
+print(f"I also said: '{y}")
+
+# Defind hilarious
+hilarious = False
+# Defind joke_evaluation, with a open ({}) valriable that can be used later.
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+# Prints joke_evaluation with the open valriable, hilarious.
+# Thats posiable becouse of the .format, that links: hilarious to the open valriable
+print(joke_evaluation.format(hilarious,))
+
+# Definds w and e as a string.
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# Prints w appended with e
+print(w + e)
+
+
+							# 2 and 3 Study Drill
+types_of_people = 10
+
+# First
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+
+# Secound
+y = f"Those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+
+# Third
+print(f"I said: {x}")
+
+# Fourth
+print(f"I also said: '{y}")
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+# Maybe it's the fifth?
+print(joke_evaluation.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
+
+
+								# 4 Study Drill
+# You just append to stirngs togther, strings are not like number and can't be "added" togther the same way.
+
+
+								# Break it!!
